7520ccaf9e9cb808623bf26394463a26d9ceac15
emlram <ramos.s.emilio@gmail.com>
2025-07-07 20:51:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+  const mensagem = `Não! Não tem como fugir;
+Não! Põe na história o seu dom, vai;
+É a hora de ir...`;
+
+  document.getElementById("mensagem").innerText = mensagem;
+}
